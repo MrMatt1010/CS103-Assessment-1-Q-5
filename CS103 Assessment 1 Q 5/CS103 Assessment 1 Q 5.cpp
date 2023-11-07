caa3691e03cc9e1ea6117e2404463d16fc39a8e4
Matt T <mrsymtec@gmail.com>
2023-11-07 15:18:49 +1300

    Started on entering new code to see if this works better

--- CS103 Assessment 1 Q 5/CS103 Assessment 1 Q 5.cpp
+++ CS103 Assessment 1 Q 5/CS103 Assessment 1 Q 5.cpp
@@ -9,7 +9,38 @@
 
 using namespace std;
 
+//Function to display the main menu
+void displayMenu() {
+    cout << "\nMain Menu:\n";
+    cout << "1.Play\n";
+    cout << "2. Final Score\n";
+    cout << "3. Exit Game\n";
+}
 
+// Function to generate a random shape and ask the user to identify it
+int playGame(int &score) {
+    string shapes[] = { "Rectangle","Triangle", "Square" };
+    int randomIndex = rand() % 3;
+    string randomShape = shapes[randomIndex];
+
+    cout << "\nIdentify the shape:\n";
+
+    if (randomShape == "Rectangle") {
+        cout << "   ________\n";
+        cout << "  |        |\n";
+        cout << "  |________|\n";
+    }
+    else if (randomShape == "Triangle") {
+        cout << "    /\\  \n";
+        cout << "   /  \\  \n";
+        cout << "  /____\\  \n";
+    }
+    else if (randomShape == "Square") {
+        cout << "  ________\n";
+        cout << " |        |\n";
+        cout << " |        |\n";
+        cout << " |________|\n";
+    }
 
     return 0;
 }
